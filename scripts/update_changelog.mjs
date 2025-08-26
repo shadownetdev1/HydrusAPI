f6f2afecc58f409857ae0a110e8bda5d9ff27df9
= <=>
2025-08-26 15:36:55 -0400

    fix release notes

--- scripts/update_changelog.mjs
+++ scripts/update_changelog.mjs
@@ -7,12 +7,15 @@ const c_path = 'CHANGELOG.MD'
 let c_text = jetpack.read(c_path)
 const t_path = 'CHANGELOG.TEMPLATE.MD'
 let t_text = jetpack.read(t_path)
+const p_path = 'TEMP_LOG.MD'
 
 const version = process.argv[2]
 const date = new Date()
 const date_string = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`
 
 u_text = u_text.replaceAll('<!-- version -->', `[${ version }](https://github.com/shadownetdev1/HyAPI/releases/tag/${version}) - ${date_string}`)
+
+jetpack.write(p_path, u_text)
 
 c_text = c_text.replace('<!-- insert_point -->', u_text)
 
